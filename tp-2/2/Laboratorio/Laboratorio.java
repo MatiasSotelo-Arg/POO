8819c096ee9d29d1dab70b03dd59c8568c0198ec
MatiasSoteloArg <matysotelo07@gmail.com>
2024-08-25 14:04:40 -0300

    add doc Laboratorio

--- tp-2/2/Laboratorio/Laboratorio.java
+++ tp-2/2/Laboratorio/Laboratorio.java
@@ -15,6 +15,13 @@ public class Laboratorio
     private int diaEntrega;
     
     //Constructor
+    /**
+    *	@param p_nombre nombre del laboratorio. 
+    *	@param p_domicilio domicilio del laboratorio . 
+    *	@param p_telefono telefono del laboratorio.
+    *	@param p_compraMin compra minima de la empresa.
+    *	@param p_diaEnt dia de entrega.
+    */
     public Laboratorio(String p_nombre, String p_domicilio, 
     String p_telefono,int p_compraMinima, int p_diaEntrega)
     {
@@ -34,56 +41,78 @@ public class Laboratorio
     }
 
     //Metodos
+    /**  @param p_nombre nombre del laboratorio. */
     private void setNombre(String p_nombre) 
     {
         this.nombre = p_nombre;
     }
     
+    /**  @param p_domicilio domicilio del laboratorio .*/
     private void setDomicilio(String p_domicilio) 
     {
         this.domicilio = p_domicilio;    
     }
     
+    /**  @param p_telefono telefono del laboratorio. */
     private void setTelefono(String p_telefono) 
     {
         this.telefono = p_telefono;
     }
     
+    /**  @param p_compraMin compra minima de la empresa. */
     private void setCompraMinima(int p_compraMinima) 
     {
         this.compraMinima = p_compraMinima;
     }
-    
+
+    /**  @param p_diaEnt dia de entrega. */
     private void setDiaEntrega(int p_diaEntrega) 
     {
         this.diaEntrega = p_diaEntrega;
     }
     
+    /** @return nombre del laboratorio. */
     public String getNombre() 
     {
         return this.nombre;
     }
-    
+
+    /** @return domicilio del laboratorio. */    
     public String getDomicilio() 
     {
         return this.domicilio;
     }
     
+    /** @return telefono del laboratorio. */
     public String getTelefono() 
     {
         return this.telefono;
     }
     
+    /**
+    *	Modifica el monto de la compra mínima.
+    *	@param p_compraMin compra minima
+    */
     public void nuevaCompraMinima(int p_compraMin) 
     {
         this.setCompraMinima(p_compraMin);
     }
     
+    /**
+    *	Permite cambiar el dia de entrega.
+    *	@param p_compraMin monto compra minima
+    */
     public void nuevoDiaEntrega(int p_diaEnt) 
     {
         this.setCompraMinima(p_diaEnt);
     }
     
+    /**
+    *	Concatena nombre del laboratorio, domicilio y telefono
+    *	del laboratorio.
+    *
+    *	@return informacion del laboratorio
+    */
     public String mostrar() 
     {
         return "Laboratorio: " + this.getNombre() + "\n" + "Domicilio: " 
