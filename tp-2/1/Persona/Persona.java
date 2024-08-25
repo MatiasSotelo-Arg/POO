0eb91cdf61ed38f0b3f840a743fb532977ed25bd
MatiasSoteloArg <matysotelo07@gmail.com>
2024-08-25 00:24:07 -0300

    Documentacion Persona Empleado

--- tp-2/1/Persona/Persona.java
+++ tp-2/1/Persona/Persona.java
@@ -1,32 +1,6 @@
-/* Persona
-    -nroDni: int
-    -nombre: String
-    -apellido: String
-    -anioNacimiento: int
-    +Persona(p_dni: int, p_nombre: String, p_apellido: String, p_anio: int)
-    -setDNI(p_dni: int): void
-    -setNombre(p_nombre: String): void
-    -setApellido(p_apellido: String): void
-    -setAnioNacimiento(p_anio: int): void
-    +getDNI(): int
-    +getNombre(): String
-    +getApellido(): String
-    +getAnioNacimiento(): int
-    +edad(): int
-    +nomYApe(): String
-    +apeYNom(): String
-    +mostrar(): void
-    
-    import java.util.*;
-    
-    Calendar fechaHoy = new GregorianCalendar();
-    int anioHoy = fechaHoy.get(Calendar.YEAR);
-*/
-
-
-
 /**
- * Write a description of class Persona here.
+ * La Clase Persona: Permite gestionar la información de una 
+ * persona y mostrarlos (nombre, apellido, dni y edad).
  * 
  * @author (Matias Sotelo) 
  * @version (21/08/2024)
@@ -44,6 +18,12 @@ public class Persona
     private int anioNacimiento;
     
     // Constructor 
+    /** 
+     * @param p_dni dni de la persona
+     * @param p_nombre de la persona
+     * @param p_apellido de la persona
+     * @param p_anio de la persona
+     */
     public Persona(int p_dni, String p_nombre, String p_apellido, int p_anio)
     {
         this.setDNI(p_dni);
@@ -53,46 +33,58 @@ public class Persona
     }
 
     // Metodos
+    /** @param p_dni dni de la persona **/
     private void setDNI(int p_dni) 
     {
         this.nroDni = p_dni;
     }
        
+    /** @param p_nombre de la persona **/
     private void setNombre(String p_nombre) 
     {
         this.nombre = p_nombre;
     }
     
+    /** @param p_apellido de la persona **/
     private void setApellido(String p_apellido)
     {
         this.apellido = p_apellido;
     }
     
+    /** @param p_anio de la persona **/
     private void setAnioNacimiento(int p_anio) 
     {
         this.anioNacimiento = p_anio;
     }
     
+    /** @return el dni de la persona **/
     public int getDNI() 
     {
         return this.nroDni;
     }
     
+    /** @return el nombre de la persona **/
     public String getNombre() 
     {
         return this.nombre;
     }
     
+    /** @return el apellido de la persona **/
     public String getApellido() 
     {
         return this.apellido;
     }
     
+    /** @return el año de nacimiento de la persona **/
     public int getAnioNacimiento()
     {
         return this.anioNacimiento;
     }
     
+    /** 
+     * Resta el año de nacimiento al año actual.
+     * 
+     * @return la edad de la persona **/
     public int edad() 
     {
         Calendar fechaHoy = new GregorianCalendar();
@@ -103,18 +95,25 @@ public class Persona
         return edad;
     }
     
+    /**  
+     * Concatena el nombre y apellido de la persona.
+     * @return el nombre y apellido **/
     public String nomYApe() 
     {   
         return this.getNombre() + " " + 
         this.getApellido();  
     }
     
+    /** 
+     * Concatena el apellido y nombre de la persona.
+     * @return apellido y nombre **/
     public String apeYNom() 
     {
         return this.getApellido() + " " +
         this.getNombre(); 
     }
     
+    /** Imprime la información de la persona Nombre, Apellido, DNI, Edad.**/
     public void mostrar() 
     {
         System.out.println("Nombre y Apellido: " + this.nomYApe() + "\nDNI: " + 
--- tp-2/5/Empleado/Empleado.java
+++ tp-2/5/Empleado/Empleado.java
@@ -1,6 +1,7 @@
 
 /**
- * Write a description of class Empleado here.
+ * La clase Empleado permite a la empresa, 
+ * la liquidación de sueldo de sus empleados.
  * 
  * @author (Sotelo Matias) 
  * @version (22/08/2024)
@@ -18,6 +19,13 @@ public class Empleado
     private int anioIngreso;
     
     //Constructor
+    /** 
+    *   @param cuil del empleado
+    *   @param apellido del empleado
+    *   @param nombre del empleado
+    *   @param sueldoBasico del empleado
+    *   @anioIngreso del empleado
+    **/
     public Empleado(long p_cuil, String p_apellido, String p_nombre, 
     double p_importe, int p_anio)
     {
@@ -79,6 +87,12 @@ public class Empleado
         return this.anioIngreso;
     }
     
+    /** 
+    *   Calcula la antiguedad del empleado 
+    *   restando el año de ingreso al año actual.
+    *   
+    *   @return la antiguedad del empleado
+    **/
     public int antiguedad() 
     {
         Calendar fechaHoy = new GregorianCalendar();
@@ -89,58 +103,109 @@ public class Empleado
         return antiguedad; 
     }
     
+    /** 
+    *   Descuenta al sueldo base un porcentaje de 
+    *   obra social y el monto de seguro de vida.
+    *   
+    *   @return el monto a descontar
+    **/
     private double descuento() 
     {
-        double descuento = (this.getSueldoBasico() * 2 / 100) + 1500;
-        
-        return descuento;
+        //monto seguro de vida
+        int aporte = 1500;
+        //porcentaje obra social
+        int porcentaje = 2;
+    
+        return (this.getSueldoBasico() * porcentaje / 100) + aporte;
     }
     
+    /** 
+    *   Obtiene el monto adicional a pagar al empleado
+    *   según sus años de antiguedad.
+    *   
+    *   @return monto adicional a pagar
+    */
     private double adicional() 
     {
-        double adicional = 0;
-        
+        //valor atributo antiguedad        
         int antiguedad = this.antiguedad(); 
+        //valor atributo sueldo base
         double sueldoBase = this.getSueldoBasico();
         
         if(antiguedad < 2) 
         {
-            adicional = sueldoBase * 2 / 100; 
+            return sueldoBase * 2 / 100; 
         } else if (antiguedad >= 2 && antiguedad < 10) 
         {
-            adicional = sueldoBase * 4 / 100;
+            return sueldoBase * 4 / 100;
         } else 
         {
-            adicional = sueldoBase * 6 / 100;
+            return sueldoBase * 6 / 100;
         }
-    
-        return adicional;
     }
     
+    /** 
+    *   Obtiene el sueldo neto sumando al sueldo basico el 
+    *   adicional y restando los descuento.
+    *
+    *   @return monto sueldo neto
+    */
     public double sueldoNeto() 
     {
         return (this.getSueldoBasico() + this.adicional()) 
         - this.descuento();
     }
 
+    /**
+    *   Concatena nombre y apellido del 
+    *   empleado.
+    *
+    *   @return nombre y apellido
+    */
     public String nomYApe() 
     {
         return this.getNombre() + " " + this.getApellido();
     }
     
+    /**
+    *   Concatena apellido y nombre del 
+    *   empleado.
+    *
+    *   @return apellido y nombre
+    */
     public String apeYNom() 
     {
         return this.getApellido() + "," + this.getNombre();
     }
     
+    /**
+    *   Imprime Información del empleado:  
+    *   Nombre, Apellido, CUIL, Antiguedad, años servicio,
+    *   Sueldo neto.
+    **/
     public void mostrar() 
     {
-        System.out.println("Nombre y Apellido: " + this.nomYApe() +
-        "\nCUIL: " + this.getCuil() + " Antiguedad: " + this.antiguedad() 
-        + " años de servicio\nSueldo Neto: $ " + this.sueldoNeto());
-        System.out.println();
+        //Metodo nombre y apellido
+        String nomYApe = this.nomYApe();
+        //Atributo cuil
+        long cuil = this.getCuil();
+        //Metodo antiguedad
+        int antiguedad = this.antiguedad();
+        //Atributo sueldo neto
+        double sueldoNeto = this.sueldoNeto();
+        
+        //Imprime Información
+        System.out.println("Nombre y Apellido: " + nomYApe +
+        "\nCUIL: " + cuil + "Antiguedad: " + antiguedad + " años de servicios" +
+        "\nSueldo Neto: $ " + sueldoNeto);
     }
     
+    /**
+    *    Concatena Cuil, Apellido, Nombre y 
+    *    Sueldo Neto del empleado.
+    * 
+    *    @return la informacion en una linea
+    */   
     public String mostrarLinea() 
     {
         return this.getCuil() + " " + this.apeYNom() + " …………… $" 
