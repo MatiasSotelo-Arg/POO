7b54f77b9a25a0ae32fefe7de9637f2265f936d2
Matias Sotelo <matysotelo07@gmail.com>
2024-09-19 20:52:11 -0300

    Add Class Curso, Ejecutable

--- Practico 4/5 Curso/CarreraEjecutable.java
+++ Practico 4/5 Curso/CarreraEjecutable.java
@@ -6,28 +6,43 @@
  * @version (a version number or a date)
  */
 
-import java.util.HashMap;
 
 public class CarreraEjecutable
 {
     public static void main() {
-        
-       HashMap<Integer, Alumno> map = new HashMap<>(); 
         
        Alumno alum1 = new Alumno(123, "Matias", "Sotelo", 10, 9);
        Alumno alum2 = new Alumno(124, "Ana", "García", 8, 7);
        Alumno alum3 = new Alumno(125, "Luis", "Pérez", 9, 10);
        Alumno alum4 = new Alumno(126, "Sofia", "Martínez", 6, 8);
        Alumno alum5 = new Alumno(127, "Carlos", "Lopez", 7, 9);
-        
-       map.put(1, alum1);
-       map.put(2, alum2);
-       map.put(3, alum3);
-       map.put(4, alum4);
-       map.put(5, alum5);
-   
-       Curso curso = new Curso("POO",map);
        
-    
+       Curso curso = new Curso("POO");
+       
+       curso.inscribirAlumno(alum1);
+       curso.inscribirAlumno(alum2);
+       curso.inscribirAlumno(alum3);
+       curso.inscribirAlumno(alum4);
+       curso.inscribirAlumno(alum5);
+       
+       System.out.println("****--Cantidad de inscriptos:" + curso.cantidadDeAlumnos());
+       curso.mostrarInscriptos();
+       
+       System.out.println("\n****-- Se da de baja a Pedro porque abandona el curso --****");
+       curso.quitarAlumno(126);
+       System.out.println("Está Sofia Martínez inscripto ?? --> " + curso.estaInscripto(126));
+       curso.mostrarInscriptos();
+       
+       
+       System.out.println("\n****-- Busca y muestra el alumno con numero de libreta 123 --****");
+       
+       if(curso.estaInscripto(123)) {
+           curso.buscarAlumno(123).mostrar();
+       }
+       
+       System.out.println("\n****-- Mostrar promedio del alumno 127 --****");
+       curso.imprimirPromedioDelAlumno(127);
+       
     }
 }
+
--- Practico 4/5 Curso/Curso.java
+++ Practico 4/5 Curso/Curso.java
@@ -7,6 +7,7 @@
  */
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Curso
 {  
@@ -19,40 +20,89 @@ public class Curso
      */
     public Curso(String p_nombre)
     {
-       this.setAlumnos(new HashMap<>());
        this.setNombreCurso(p_nombre);
+       this.setAlumnos(new HashMap<>());
     }
 
     public Curso(String p_nombre, HashMap p_alumnos)
     {
-        
-        if(this.cantidadDeAlumnos() != 0) {
-            
-            for (Object alumno : p_alumnos.keySet()) {
-                this.inscribirAlumno(alumno);
-            }
-            
-        } else {
-
-            this.setNombreCurso(p_nombre);
-            this.setAlumnos(new HashMap<>());
-        }
-        
+        this.setNombreCurso(p_nombre);
+        this.setAlumnos(p_alumnos);
     }
     
     //Setter
-    public void setNombreCurso(String p_nombre) 
+    private void setNombreCurso(String p_nombre) 
     {
         this.nombre = p_nombre;
     }
     
-    public void setAlumnos(HashMap<Integer, Alumno> p_alumno) 
+    private void setAlumnos(HashMap<Integer, Alumno> p_alumno) 
     {
         this.alumnos = p_alumno;
     }
     
     //Getter
-    
+    public HashMap<Integer, Alumno> getAlumnos() {
+        return this.alumnos;  
+    }
+
     //Metodos
+    public void inscribirAlumno(Alumno p_alumno) 
+    {
+        this.getAlumnos().put(new Integer(p_alumno.getLu()), p_alumno );
+    }
     
+    public Alumno quitarAlumno(int p_lu) 
+    {
+        return this.getAlumnos().remove(p_lu);
+    }
+    
+    public int cantidadDeAlumnos()
+    {
+        return this.getAlumnos().size();
+    }
+    
+    public boolean estaInscripto(int p_lu) 
+    {
+        return this.buscarAlumno(p_lu) != null;
+    }
+    
+    public boolean estaInscripto(Alumno p_alumno)
+    {
+        return this.getAlumnos().get(p_alumno) != null;
+    }
+    
+    public Alumno buscarAlumno(int p_lu) 
+    {
+        return this.getAlumnos().get(p_lu);
+    }
+    
+    public void imprimirPromedioDelAlumno(int p_lu)
+    {
+        Alumno alumno = this.buscarAlumno(p_lu);
+        
+        double n1 = alumno.getNota1(); 
+        double n2 = alumno.getNota2();
+        
+        double promedio = alumno != null ? n1 + n2 / 2 : -1; 
+        
+        if (promedio != -1) 
+        {
+            System.out.println("Promedio: " + promedio);
+        } else 
+        {
+            System.out.println("No se encuentra el alumno");
+        }   
+    }
+    
+    public void mostrarInscriptos()
+    {
+        for(Map.Entry<Integer, Alumno> e : this.getAlumnos().entrySet()) 
+        {
+            Integer lu = e.getKey();
+            String alumno = e.getValue().nomYApe();
+
+            System.out.println(lu + " " + alumno);
+        }
+    }
 }
